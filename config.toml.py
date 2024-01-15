2edf3482dbfbb5c36700b70ccc8acfa9914cb4dc
IMAGINEDATA1 <156206757+IMAGINEDATA1@users.noreply.github.com>
2024-01-15 17:24:42 +0100

    Add files via upload

---
+++ config.toml.py
@@ -0,0 +1,22 @@
+
+# Primary accent for interactive elements
+primaryColor = '#7792E3'
+
+# Background color for the main content area
+backgroundColor = '#273346'
+
+# Background color for sidebar and most interactive widgets
+secondaryBackgroundColor = '#B9F1C0'
+
+# Color used for almost all text
+textColor = '#2c3e50'
+
+# Font family for all text in the app, except code blocks
+# Accepted values (serif | sans serif | monospace) 
+# Default: "sans serif"
+font = "sans serif"
+
+background-image = radial-gradient(#444cf7 0.5px, #e5e5f7 0.5px)
+    
+background-size = 10px 10px
+
