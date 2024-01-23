20beecf9d2aa9ea8dcab8b3e2b7f02f01760e64e
IMAGINEDATA1 <156206757+IMAGINEDATA1@users.noreply.github.com>
2024-01-23 15:38:38 +0100

    streamlit_imagindata.py

--- streamlit_imagindata.py
+++ streamlit_imagindata.py
@@ -1,9 +1,8 @@
 import streamlit as st
-from streamlit.components.v1 import components
-from googletrans import Translator
 import pandas as pd
-from sklearn.neighbors import NearestNeighbors
 import requests
+from sklearn.neighbors import NearestNeighbors
+from googletrans import Translator
 import random
 
 # Définir le thème personnalisé
@@ -25,7 +24,7 @@ def get_movie_details(movie_id):
     api_key = "db38952c66997974559ef641200fc25e"
     base_url = "https://api.themoviedb.org/3/movie/"
     endpoint = str(movie_id)
-    url = f"{base_url}{endpoint}?api_key={api_key}"
+    url = f"{base_url}{endpoint}?api_key={api_key}&append_to_response=credits"
 
     response = requests.get(url)
     if response.status_code == 200:
@@ -34,10 +33,16 @@ def get_movie_details(movie_id):
         return None
 
 # Fonction pour afficher les résultats de recommandation de manière structurée
-def display_recommendations(neighbors_names):
+def display_recommendations(neighbors_indices, df_KNN):
     st.subheader(f"Résultats de recommandation:")
-    for i, movie_title in enumerate(neighbors_names, start=1):
+    for i, index in enumerate(neighbors_indices, start=1):
+        movie_title = df_KNN.loc[index, 'primaryTitle']
         st.write(f"{i}. {movie_title}")
+        # Afficher les caractéristiques du film recommandé
+        st.write(f"   Caractéristiques: {df_KNN.loc[index, ['startYear', 'original_language', 'Action', 'Adventure', 'Biography', 'Crime', 'Mystery']]}")
+        # Requête API pour obtenir les détails du film recommandé
+        movie_details = get_movie_details(df_KNN.loc[index, 'tconst'])
+        display_movie_details(movie_details)
 
 # Fonction pour afficher les détails du film à partir de l'API TMDb
 def display_movie_details(movie_details):
@@ -50,10 +55,12 @@ def display_movie_details(movie_details):
         st.write(f"**Nombre de votes:** {movie_details.get('vote_count')}")
         st.write(f"**Durée:** {movie_details.get('runtime')} minutes")
         st.write(f"**Genre:** {', '.join([genre['name'] for genre in movie_details.get('genres', [])])}")
+        
         st.write("**Acteurs:**")
         cast_members = movie_details.get('credits', {}).get('cast', [])[:3]
         for cast_member in cast_members:
             st.write(f"- {cast_member.get('name')}")
+
         st.write("**Réalisateurs:**")
         crew_members = movie_details.get('credits', {}).get('crew', [])
         directors = [crew_member.get('name') for crew_member in crew_members if crew_member.get('job') == 'Director']
@@ -71,29 +78,29 @@ def main():
     Essayez de cliquer sur le bouton de traduction pour voir le contenu dans une autre langue!
     """
 
-    # Affichage du contenu initial
-    st.markdown(page_content)
-
     # Bouton de traduction
-    if st.button("Traduire en anglais"):
-        translated_content = translate_page(page_content, target_language='en')
-        st.markdown(translated_content)
+    current_language = st.session_state.get('current_language', 'fr')
+    if st.button("Traduire"):
+        st.session_state.current_language = 'en' if current_language == 'fr' else 'fr'
+
+    # Affichage du contenu traduit
+    page_content_translated = translate_page(page_content, target_language=current_language)
+    st.markdown(page_content_translated)
 
     # Charger le DataFrame depuis l'URL
     df_KNN = pd.read_csv("https://raw.githubusercontent.com/IMAGINEDATA1/APP/main/t_KNN")
 
-    # Barre de recherche avec autocomplétion
-    user_input_film = st.text_input("Tapez votre recherche", df_KNN['primaryTitle'].iloc[0])
+    # Barre de recherche pour la recommandation
+    user_input_film = st.text_input("Recherchez par titre, acteur ou réalisateur", df_KNN['primaryTitle'].iloc[0])
 
     # Vérifier si la recherche dans 'primaryTitle' ne donne pas de résultats
     if user_input_film not in df_KNN['primaryTitle'].values:
-    # Si la recherche ne donne pas de résultats, essayer dans 'primaryName'
-       user_input_film = st.text_input("Tapez votre recherche", df_KNN['primaryName'].iloc[0])
+        # Si la recherche ne donne pas de résultats, essayer dans 'primaryName'
+        user_input_film = st.text_input("Tapez votre recherche", df_KNN['primaryName'].iloc[0])
 
     # Vérifier à nouveau si la recherche dans 'primaryName' ne donne pas de résultats
-       if user_input_film not in df_KNN['primaryName'].values:
+    if user_input_film not in df_KNN['primaryName'].values:
         st.warning("Aucun résultat trouvé pour la recherche spécifiée.")
-
 
     if user_input_film:
         user_film_features = df_KNN.loc[df_KNN['primaryTitle'] == user_input_film, ['startYear', 'original_language', 'Action', 'Adventure', 'Biography', 'Crime', 'Mystery']]
@@ -115,23 +122,15 @@ def main():
         filtered_neighbors_indices = [index for index in filtered_neighbors_indices if index != df_KNN[df_KNN['primaryTitle'] == user_input_film].index[0]]
 
         # Résultats recommandations
-        neighbors_names = df_KNN['primaryTitle'].iloc[filtered_neighbors_indices]
-        display_recommendations(neighbors_names)
-
-        # Requête API pour obtenir les détails du film sélectionné
-        movie_details = get_movie_details(df_KNN.loc[df_KNN['primaryTitle'] == user_input_film, 'tconst'].values[0])
-        display_movie_details(movie_details)
+        display_recommendations(filtered_neighbors_indices, df_KNN)
 
     else:
         # Si le film n'a pas été trouvé
         st.write(f"\nLe film '{user_input_film}' n'a pas été trouvé dans la base de données.")
 
         # 4 films choisis aléatoirement comme recommandations
-        random_recos = random.sample(df_KNN['primaryTitle'].tolist(), 4)
-
-        st.write("Vous ne trouvez pas ? Voici quelques unes de mes idées :")
-        for i, movie_title in enumerate(random_recos, start=1):
-            st.write(f"{i}. {movie_title}")
+        random_recos_indices = random.sample(range(len(df_KNN)), 4)
+        display_recommendations(random_recos_indices, df_KNN)
 
     # SOUS-TITRE
     st.subheader("Bonne séance ! 🍿🍿🍿 ")
