7331c8fd770dcae02db49697f5e1457e3d8f9c5b
IMAGINEDATA1 <156206757+IMAGINEDATA1@users.noreply.github.com>
2024-01-29 22:24:08 +0100

    Update streamlit_imagindata_nlp.py

--- streamlit_imagindata_nlp.py
+++ streamlit_imagindata_nlp.py
@@ -1,81 +1,100 @@
 import streamlit as st
-import pandas as pd
-import nltk
-from nltk.corpus import stopwords
-import re
-import string
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.stem.snowball import SnowballStemmer
-from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pickle
+import requests
 
-nltk.download('punkt')
-nltk.download('stopwords')
-
-# Charger les DataFrames depuis l'URL
+# Charger les données
+movies = pickle.load(open("movies_list_tuto.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
 df_actors = pd.read_csv("https://raw.githubusercontent.com/IMAGINEDATA1/APP/main/t_algo_actors")
 df_directors = pd.read_csv("https://raw.githubusercontent.com/IMAGINEDATA1/APP/main/t_algo_directors")
 df_NLP = pd.read_csv("https://raw.githubusercontent.com/IMAGINEDATA1/APP/main/t_mainNLP")
 df_prod = pd.read_csv("https://raw.githubusercontent.com/IMAGINEDATA1/APP/main/t_algo_prod")
 df_matrice = pd.read_pickle("https://raw.githubusercontent.com/IMAGINEDATA1/APP/main/df_matrice.pkl")
 
-# Fonction de nettoyage
-def clean(text):
-    tokens = nltk.word_tokenize(text.lower())
-    tokens_clean = []
-
-    additional_stopwords = ["'s", "ca", "n't"]
-    stopwordsenglish = set(stopwords.words('english') + additional_stopwords)
-
-    for word in tokens:
-        word = word.strip(string.punctuation)
-        if word and word not in stopwordsenglish and not re.match(r'^\W+$', word):
-            tokens_clean.append(word)
-    cleaned_text = ' '.join(tokens_clean)
-    return cleaned_text
-
-df_NLP['tags_NLP'] = df_NLP['tags_NLP'].apply(clean)
-
-# Fonction de stemming
-def stem(text):
-    y = []
-    ps = SnowballStemmer("english")
-    for i in text.split():
-        y.append(ps.stem(i))
-    return " ".join(y)
-
-df_NLP['tags_NLP'] = df_NLP['tags_NLP'].apply(stem)
-
-# Mesure de la similarité entre 2 vecteurs
-from sklearn.metrics.pairwise import cosine_similarity
-
-cv = CountVectorizer()
-vectors = cv.fit_transform(df_NLP['tags_NLP']).toarray()
-similarity = cosine_similarity(vectors)
-
-def main():
-    st.title("App de Recommandation de Films")
-
-    # Ajoutez les composants Streamlit pour la recherche et l'affichage des résultats
-    user_input = st.text_input("Rechercher des films similaires :")
-    if st.button("Rechercher"):
-        # Nettoyage et stemming du texte d'entrée
-        cleaned_input = stem(clean(user_input))
-
-        # Mesure de la similarité avec les films existants
-        matching_movies = df_NLP[df_NLP["primaryTitle"].str.contains(cleaned_input, case=False, na=False)]
-        if not matching_movies.empty:
-            movie_indices = matching_movies.index
-            distances = np.median(similarity[movie_indices], axis=0)
-            sorted_indices = np.argsort(distances)[::-1]
-            num_recommendations = min(5, len(sorted_indices))
-            movies_list = sorted_indices[1:num_recommendations + 1]
-
-            # Affichage des résultats
-            st.subheader("Films recommandés:")
-            for i in movies_list:
-                st.write(df_NLP.iloc[i].primaryTitle)
-        else:
-            st.warning(f"Aucun film trouvé avec le mot-clé '{cleaned_input}'.")
-
-if __name__ == "__main__":
-    main()
+# Fonction pour récupérer les posters à partir de l'API TMDb
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=db38952c66997974559ef641200fc25e&append_to_response=credits".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+# Fonction de recommandation par titre de film
+def recommend_similar_movies_by_title(title):
+    matching_movies = movies[movies['primaryTitle'].str.contains(title, case=False, na=False)]
+    if not matching_movies.empty:
+        index = matching_movies.index
+        distances = np.median(similarity[index], axis=0)
+        sorted_indices = np.argsort(distances)[::-1]
+        num_recommendations = min(5, len(sorted_indices))
+        movies_list = sorted_indices[1:num_recommendations + 1]
+        recommend_movie = []
+        recommend_poster = []
+        for i in movies_list:
+            movie_id = movies.iloc[i].tconst
+            recommend_movie.append(movies.iloc[i].primaryTitle)
+            recommend_poster.append(fetch_poster(movie_id))
+        return recommend_movie, recommend_poster
+    else:
+        st.warning(f"Aucun film trouvé avec le titre '{title}'.")
+# Fonction de recommandation par genre
+def recommend_similar_movies_by_genre(genre):
+    matching_movies_genre = movies[movies['genres'].str.contains(genre, case=False, na=False)]
+    if not matching_movies_genre.empty:
+        index = matching_movies_genre.index
+        distances = np.median(similarity[index], axis=0)
+        sorted_indices = np.argsort(distances)[::-1]
+        num_recommendations = min(5, len(sorted_indices))
+        movies_list = sorted_indices[1:num_recommendations + 1]
+        recommend_movie = []
+        recommend_poster = []
+        for i in movies_list:
+            movie_id = movies.iloc[i].tconst
+            recommend_movie.append(movies.iloc[i].genres)
+            recommend_poster.append(fetch_poster(movie_id))
+        return recommend_movie, recommend_poster
+    else:
+        st.warning(f"Aucun film trouvé avec le genre '{genre}'.")
+# Fonction de recommandation par acteur
+def recommend_similar_movies_by_actor(actor):
+    matching_movies_actor = movies[movies['actors'].str.contains(actor, case=False, na=False)]
+    if not matching_movies_actor.empty:
+        index = matching_movies_actor.index
+        distances = np.median(similarity[index], axis=0)
+        sorted_indices = np.argsort(distances)[::-1]
+        num_recommendations = min(5, len(sorted_indices))
+        movies_list = sorted_indices[1:num_recommendations + 1]
+        recommend_movie = []
+        recommend_poster = []
+        for i in movies_list:
+            movie_id = movies.iloc[i].tconst
+            recommend_movie.append(movies.iloc[i].genres)
+            recommend_poster.append(fetch_poster(movie_id))
+        return recommend_movie, recommend_poster
+    else:
+        st.warning(f"Aucun film trouvé avec le genre '{actor}'.")
+# Titre de la page
+st.header("Movie Recommender System")
+# Options de recherche
+search_options = ["Titre", "Genre", "Acteur"]
+search_option = st.selectbox("Choisir une option de recherche", search_options)
+# Champ de saisie en fonction de l'option choisie
+user_input = st.text_input(f"Entrez le {search_option.lower()}", "")
+# Bouton de recherche
+if st.button("Rechercher"):
+    if user_input:
+        if search_option == "Titre":
+            movie_name, movie_poster = recommend_similar_movies_by_title(user_input)
+        elif search_option == "Genre":
+            movie_name, movie_poster = recommend_similar_movies_by_genre(user_input)
+        elif search_option == "Acteur":
+            movie_name, movie_poster = recommend_similar_movies_by_actor(user_input)
+        # Affichage des recommandations
+        col1, col2, col3, col4, col5 = st.columns(5)
+        for i in range(min(5, len(movie_name))):
+            with col1:
+                st.text(movie_name[i])
+                st.image(movie_poster[i])
+    else:
+        st.warning("Veuillez entrer une valeur de recherche.")
